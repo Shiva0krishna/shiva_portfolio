1099c33527335991d1f3da55918b2278d090afbe
S.shiva krishna reddy <128881036+Shiva0krishna@users.noreply.github.com>
2024-09-07 17:08:00 +0530

    version conflict in dependencies issue

--- src/components/About/Leetcode.js
+++ src/components/About/Leetcode.js
@@ -1,38 +1,38 @@
 import React from 'react'
 import { Row } from 'react-bootstrap'
-import LeetCodeCalendar from 'leetcode-calendar';
+// import LeetCodeCalendar from 'leetcode-calendar';
 
 export default function Leetcode() {
-    const exampleTheme = {
-    light: [
-      'rgb(235, 235, 235)',  
-      'rgba(144, 138, 144, 0.44)',
-      'rgba(60, 179, 113, 0.6)',  
-      'rgba(34, 139, 34, 0.76)',  
-      'rgba(0, 100, 0, 0.92)',    
-    ],
-    dark: [
-      'rgb(235, 235, 235)',
-      'rgba(100, 238, 100, 0.64)',
-      'rgba(60, 179, 113, 0.6)',  
-      'rgba(34, 145, 34, 0.76)',  
-      'rgba(0, 180, 0, 0.92)',    
-    ],
-  };
+    // const exampleTheme = {
+    // light: [
+    //   'rgb(235, 235, 235)',  
+    //   'rgba(144, 138, 144, 0.44)',
+    //   'rgba(60, 179, 113, 0.6)',  
+    //   'rgba(34, 139, 34, 0.76)',  
+    //   'rgba(0, 100, 0, 0.92)',    
+    // ],
+    // dark: [
+    //   'rgb(235, 235, 235)',
+    //   'rgba(100, 238, 100, 0.64)',
+    //   'rgba(60, 179, 113, 0.6)',  
+    //   'rgba(34, 145, 34, 0.76)',  
+    //   'rgba(0, 180, 0, 0.92)',    
+    // ],
+  //};
   return (
     <div>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
       <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
        <strong className="white">LeetCodeCalendar</strong>
       </h1>
-      <LeetCodeCalendar
+      {/* <LeetCodeCalendar
         username='shiva_sangati' 
         blockSize={16}
         blockMargin={2.6} 
         fontSize={24} 
         theme={exampleTheme} 
         style={{ maxWidth: '1060px' ,marginLeft:"7%"}} 
-      />
+      /> */}
     </Row>
     </div>
   )
